6f83a6d91184feb794a5406157bb6fa7b3c7ed8a
Nicolas Diaz Calens <ndiazcalens@gmail.com>
2024-10-18 12:34:57 -0300

    Añadiendo archivo vercel

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 class DevelopmentConfig():
     DEBUG = True
-    MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")  # Usa localhost como valor por defecto para desarrollo local
-    MYSQL_USER = os.getenv("MYSQL_USER", "root")        # Usa "root" como valor por defecto para desarrollo local
-    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "")     # Usa una cadena vacía como valor por defecto
+    MYSQL_HOST = os.getenv("(la URL o IP de tu base de datos en Railway)", "localhost")  # Usa localhost como valor por defecto para desarrollo local
+    MYSQL_USER = os.getenv("(el usuario de la base de datos)", "root")        # Usa "root" como valor por defecto para desarrollo local
+    MYSQL_PASSWORD = os.getenv("(la contraseña de la base de datos)", "")     # Usa una cadena vacía como valor por defecto
     MYSQL_DB = os.getenv("MYSQL_DB", "tasks_db")        # Usa "tasks_db" como valor por defecto para desarrollo local
 
 config = {
